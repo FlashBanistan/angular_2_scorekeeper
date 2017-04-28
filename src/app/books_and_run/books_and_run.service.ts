01ab7904d82bee97d6d7c57092cda4ddd82a35c6
Aaron Chesley <achesley@crossroadstech.net>
2017-04-28 15:02:39 -0600

    move game logic to service

--- src/app/books_and_run/books_and_run.service.ts
+++ src/app/books_and_run/books_and_run.service.ts
@@ -127,21 +127,91 @@ export class BooksAndRunService {constructor(private http: Http){}
       // return "Game deleted succesfully!"
     }
 
-
-    gameFinished(game) {
-      for(var i in game.players) {
-        if(!game.players[i].scores.isComplete()) {
+    /* Loops through an array of players to check if scores are complete */
+    isGameFinished(players) {
+      for(var i in players) {
+        if(!players[i].scores.isComplete()) {
           return false;
         }
       }
       return true;
     }
 
-    savePlayerStats(player) {
+
+    /*
+    Determines total score and number of rounds won for each player.
+    Accepts an array of player objects.
+    Returns an array of scored player objects with 'pk', 'score', and 'num_hands_won'.
+    */
+    generatePlayersStats(players) {
+      var newPlayers = [];
+      players.forEach(function(player) {
+        let roundsWon = 0;
+        for(let score in player.scores) {
+          if(player.scores.hasOwnProperty(score)) {
+            if(player.scores[score] == 0) {
+              roundsWon +=1;
+            }
+          }
+        }
+        let tempPlayer = {
+          "pk": player.pk,
+          "score": player.scores.getTotal(),
+          "num_hands_won": roundsWon,
+        }
+        newPlayers.push(tempPlayer);
+      });
+      return newPlayers
+    }
+
+
+    /*
+    Determines winner of the game.
+    Accepts an array of scored player objects.
+    Returns a player object.
+    */
+    getWinner(scoredPlayers) {
+      var lowScore = scoredPlayers[0].score;
+      var winner = scoredPlayers[0]
+      for(var i=0; i<scoredPlayers.length; i++) {
+        if(scoredPlayers[i].score < lowScore) {
+          winner = scoredPlayers[i]
+          lowScore = scoredPlayers[i].score
+        }
+      }
+      return winner
+    }
+
+
+    /*
+    Assign winner or loser.
+    Accepts an array of scored player objects.
+    Return an array of scored player objects adding winner = true/false.
+    */
+    assignWinOrLose(scoredPlayers) {
+      var winner = this.getWinner(scoredPlayers);
+      scoredPlayers.forEach(function(player) {
+        if(player.pk != winner.pk) {
+          player['is_winner'] = false;
+        } else {
+          player['is_winner'] = true;
+        }
+      })
+      return scoredPlayers
+    }
+
+    /*
+    Record player statistics to the database.
+    Accepts a scored player object with win or lose assigned.
+    Returns null
+    */
+    recordStats(scoredPlayer) {
       let headers = new Headers({ 'Content-Type': 'application/json' });
 
-      return this.http.put('http://localhost:8000/api/books_and_run/statistics/' + player.pk + '/', JSON.stringify(player), {headers: headers});
+      return this.http.put('http://localhost:8000/api/books_and_run/statistics/' + scoredPlayer.pk + '/', JSON.stringify(scoredPlayer), {headers: headers});
     }
+
+
 
 
 }
--- src/app/books_and_run/play/books_and_run_play.component.ts
+++ src/app/books_and_run/play/books_and_run_play.component.ts
@@ -23,16 +23,16 @@ export class BooksAndRunPlayComponent implements OnInit, AfterViewChecked {
     this.toastr.setRootViewContainerRef(vRef);
   }
 
-  game = { players: []};
+  game = { players: [] };
 
 
   ngOnInit(): void {
     // Either restore game or create a new one:
-    if(localStorage.getItem('game') === null) {
+    if (localStorage.getItem('game') === null) {
       this.game = this.booksAndRunService.prepareGame();
       this.booksAndRunService.saveGame(this.game);
     } else {
-        this.game = this.booksAndRunService.restoreGame();
+      this.game = this.booksAndRunService.restoreGame();
     };
   }
 
@@ -43,75 +43,38 @@ export class BooksAndRunPlayComponent implements OnInit, AfterViewChecked {
   }
 
 
-  recordStats(game) {
-
-    // Check that the game is finished:
-    if(this.booksAndRunService.gameFinished(game)) {
-
-      // Determine total score and number of rounds won for each player:
-      var players = [];
-      game.players.forEach(function(player) {
-        var tempPlayer = {};
-        var roundsWon = 0;
-        for(var score in player.scores) {
-          if(player.scores.hasOwnProperty(score)) {
-            if(player.scores[score] == 0) {
-              roundsWon += 1;
-            }
-          }
-        }
-        tempPlayer['pk'] = player.pk;
-        tempPlayer['score'] = player.scores.getTotal();
-        tempPlayer['num_hands_won'] = roundsWon;
-        players.push(tempPlayer)
-      })
-
-      // Determine winner of game:
-      var lowScore = players[0].score;
-      var winner = players[0].pk
-      for(var i=0; i<players.length; i++) {
-        if(players[i].score < lowScore) {
-          winner = players[i].pk
-          lowScore = players[i].score
-        }
-      }
-
-      // Assign winner true or false to each player:
-      players.forEach(function(player) {
-        if(player.pk === winner) {
-          player['is_winner'] = true;
-        }
-        else {
-          player['is_winner'] = false;
-        }
-      })
-
-      // Push the stats to the database:
-      for(var i=0; i<players.length; i++) {
-        this.booksAndRunService.savePlayerStats(players[i])
-          .map(
-            (res => res.json()),
-          )
-          .subscribe(
-            (result => this.toastr
-              .success(result.user.username + ', has been saved.', 'Success!', {toastLife: 5000, showCloseButton: false})),
-            (error => this.toastr
-              .error(error.statusText + '.  Write your stats down before resetting the game.', 'Failure!', {toastLife: 5000, showCloseButton: false}))
-          )
-      }
-
-    }
-    else this.toastr.warning('Please finish the game.');
-  }
-
   resetGame() {
     this.game.players.forEach(function(player) {
       player.scores = new Score("");
     })
   }
 
+
   createNewGame() {
     // this.booksAndRunService.deleteGame()
+  }
+
+
+  finishGame(game) {
+    if (!this.booksAndRunService.isGameFinished(game.players)) return this.toastr.warning('Please finish the game.');
+
+    var scoredPlayers = this.booksAndRunService.generatePlayersStats(game.players);
+    var winner = this.booksAndRunService.getWinner(scoredPlayers);
+
+    scoredPlayers = this.booksAndRunService.assignWinOrLose(scoredPlayers);
+
+    for (var i = 0; i < scoredPlayers.length; i++) {
+      this.booksAndRunService.recordStats(scoredPlayers[i])
+        .map(
+        (res => res.json()),
+      )
+        .subscribe(
+        (result => this.toastr
+          .success(result.user.username + ', has been saved.', 'Success!', { toastLife: 5000, showCloseButton: false })),
+        (error => this.toastr
+          .error(error.statusText + '.  Write your stats down before resetting the game.', 'Failure!', { toastLife: 5000, showCloseButton: false }))
+        )
+    }
   }
 
 
